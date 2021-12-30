abb415ed193abce71ca17a577b6d7a0a3b4bfb02
Azhar <3157918+zhar97@users.noreply.github.com>
2021-12-30 09:49:16 +0800

    Update Shopee spending check with `completed` and `refunded`
    list_type: {3, 4}

--- shopee_spent_check.py
+++ shopee_spent_check.py
@@ -5,35 +5,41 @@ headers = {
     'cookie': 'SPC_EC={SPC_EC}'.format(SPC_EC=argv[-1])
 }
 
-total = 0
-order = 0
-params = (('list_type', 4),)
-offset = 0
+totals = []
+for list_type in (3, 4):
+    total = 0
+    order = 0
+    params = (('list_type', list_type),)
+    offset = 0
 
-while offset >= 0:
-    response = get(
-        'https://shopee.com.my/api/v4/order/get_order_list', 
-        headers=headers, 
-        params=params + (('offset', offset),)
-    )
+    while offset >= 0:
+        response = get(
+            'https://shopee.com.my/api/v4/order/get_order_list', 
+            headers=headers, 
+            params=params + (('offset', offset),)
+        )
     
-    dat = response.json()['data']
-    if dat.get('details_list'):
-        for details in dat['details_list']:
-            for value in details.values():
-                if isinstance(value, dict) and value.get('info_card'):
-                    print('found')
-                    order_total = value['info_card']['final_total'] / 100000
+        dat = response.json()['data']
+    
+        if dat.get('details_list'):
+            for details in dat['details_list']:
+                if isinstance(details, dict) and details.get('info_card'):
+                    order_total = details['info_card']['final_total'] / 100000
                     total += order_total
                     order += 1
                     print(
                         order, ': ',
-                        'RM ', total_order,
+                        'RM ', order_total,
                         ' - ', 
-                        value['info_card']['order_list_cards'][0]['items'][0]['name'],
+                        details['info_card']['order_list_cards'][0]['items'][0]['name'],
                         sep=''
                     )
-    offset = dat.get('next_offset', -1)
+        offset = dat.get('next_offset', -1)
+    
+    totals.append(total)
+    print('TOTAL: RM', round(total, 2), 
+          end=2*'\n')
 
-print('Calculation completed!')
-print('GRAND TOTAL: RM', round(total * 100) / 100)
+print('Calculation Completed!')
+print('GRAND TOTAL: RM', round(totals[0], 2), 
+      '+ RM', round(totals[1], 2), '(refunded)')
